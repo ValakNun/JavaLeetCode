b49ef6b7071fd2c0ec1d6a37c7a60d8060dfc5b6
ValakNun <82223400+ValakNun@users.noreply.github.com>
2022-11-20 00:21:51 +0530

    Code for Reverse String Added
    Code for Reverse String Added

--- Integers/RootOfANumber.java
+++ Integers/RootOfANumber.java
@@ -1,8 +1,9 @@
 /* Calculate Root of a number using Newton-Raphson method */
+/* Square Root of a Number; Leetcode: https://leetcode.com/problems/sqrtx/ */
 public class RootOfANumber {
     public static double NthRootOfNumber(double b, long n){
         double result = 1;
-        int number_of_iterations  = 5;
+        int number_of_iterations  = 20;
         while (number_of_iterations != 0){
             result = (b + (n-1) * Math.pow(result, n))/(n * Math.pow(result, n-1));
             number_of_iterations -= 1;
@@ -10,7 +11,7 @@ public class RootOfANumber {
         return result;
     }
     public static void main(String[] args){
-        System.out.println(NthRootOfNumber(5, 5));
+        System.out.println(NthRootOfNumber(625, 2));
     }
     
 }
---
+++ Strings/ReverseString.java
@@ -0,0 +1,19 @@
+/* Leetcode : https://leetcode.com/problems/reverse-string/ */
+class Solution {
+    public static char[] reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while(left <= right){
+            char temp = s[left] ;
+            s[left] = s[right] ;
+            s[right] = temp ;
+            left += 1;
+            right -= 1;
+        }
+        return s;
+    }
+    public static void main(String[] args){
+        System.out.println(reverseString(new char[]{'h','e','l','l','o'}));
+        System.out.println(reverseString(new char[]{'h','a','n','n','a','H'}));
+    }
+}
